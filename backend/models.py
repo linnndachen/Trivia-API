aa323aad9adb7074c4e7f0af4483cb961d1e177c
wchen928 <lchen.msc2019@ivey.ca>
2020-07-19 10:48:23 -0700

    feat: add relationship between the two tables

--- backend/models.py
+++ backend/models.py
@@ -29,7 +29,7 @@ class Question(db.Model):
   id = Column(Integer, primary_key=True)
   question = Column(String)
   answer = Column(String)
-  category = Column(String)
+  category = Column(Integer, db.ForeignKey('categories.id'), nullable=False)
   difficulty = Column(Integer)
 
   def __init__(self, question, answer, category, difficulty):
@@ -67,6 +67,7 @@ class Category(db.Model):
 
   id = Column(Integer, primary_key=True)
   type = Column(String)
+  questions = db.relationship('Question', cascade="all, delete", backref="question", lazy=True)
 
   def __init__(self, type):
     self.type = type
